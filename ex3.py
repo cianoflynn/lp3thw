6d8f6ff05172d938b7f20b6733eb099fe59da113
cianoflynn <cianoflynn@gmail.com>
2019-04-24 15:15:19 +0100

    Add earlier exercises

---
+++ ex3.py
@@ -0,0 +1,26 @@
+# String
+print("I will count my chickens:")
+# String combined with ints pemdas parenthesis, exponents, multiplication, division, addition, subtraction
+print("Hens", 25 + 30 / 6)
+# String. Mult before div before minux
+print("Roosters", 100 - 25 * 3 % 4)
+# string
+print("Now I will count the eggs:")
+# pemdas again
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# string
+print("Is it true that 3 + 2 < 5 - 7?")
+# + - then < 
+print(3 + 2 < 5 - 7)
+# string plus  equation
+print("What is 3 + 2?", 3 + 2)
+# string then, equation
+print("What is 5 - 7", 5 - 7)
+# 
+print("Oh, that's why it's false.")
+
+print("How about some more.")
+
+print("Is it greater?", -5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
---
+++ ex5.py
@@ -0,0 +1,20 @@
+name = "Cian S. O'Flynn"
+age = 40
+height = 74 # inches
+cmheight = height*2.54
+weight = 200
+eyes = 'Blue'
+teeth = 'Coffee Yellow'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {cmheight} cm's tall.")
+print(f"He's {weight} pounds heavy.")
+print(f"Actually that's slighly heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
